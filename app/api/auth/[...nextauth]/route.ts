c0ca3fb519626fbde0e9e6cc3f08236cceb2c620
Dustin Schwanger <dustinschwanger@Dustin-Schwanger-MBA.local>
2025-08-25 13:37:59 -0400

    Login first step Corrected

--- app/api/auth/[...nextauth]/route.ts
+++ app/api/auth/[...nextauth]/route.ts
@@ -5,7 +5,7 @@ import { prisma } from "@/lib/prisma"
 import bcrypt from "bcryptjs"
 import type { NextAuthOptions } from "next-auth"
 
-export const authOptions: NextAuthOptions = {
+const authOptions: NextAuthOptions = {
   adapter: PrismaAdapter(prisma) as any,
   providers: [
     CredentialsProvider({
